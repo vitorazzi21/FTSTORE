edbf84115e6fcd53ae1fc74e6cdb3d8892dd737c
João Octavio Vitorazzi <vitorazzijoaooctavio@gmail.com>
2026-01-31 22:37:51 -0300

    Fiz o carrousel das camisas , falta movimentar a legenda tbm

---
+++ js/carrousel.js
@@ -0,0 +1,30 @@
+//carousel é a maquina que faz mexer//
+//cards_camisas é a pista//
+//camisas_cards são os produtos//
+
+
+document.querySelectorAll('.carousel-wrapper').forEach(wrapper => {
+  const track = wrapper.querySelector('.cards_camisas');
+  const rightBtn = wrapper.previousElementSibling.querySelector('.arrow.right');
+  const leftBtn  = wrapper.previousElementSibling.querySelector('.arrow.left');
+
+  let position = 0;
+
+  const cardWidth = 370; // 300 + 70
+  const visibleCards = 4;
+  const maxScroll = track.children.length - visibleCards;
+
+  rightBtn.addEventListener('click', () => {
+    if (position < maxScroll) {
+      position++;
+      track.style.transform = `translateX(-${position * cardWidth}px)`;
+    }
+  });
+
+  leftBtn.addEventListener('click', () => {
+    if (position > 0) {
+      position--;
+      track.style.transform = `translateX(-${position * cardWidth}px)`;
+    }
+  });
+});
